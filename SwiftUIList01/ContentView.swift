370d389cb91b1340b1f8b38e5c4a1678dc74f127
Mamoru Sugihara <sugiharam2@gmail.com>
2021-02-16 20:58:43 +0900

    Add selection with a tap

--- SwiftUIList01/ContentView.swift
+++ SwiftUIList01/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State var selected = 0
+
 	let items = (0..<10).map { $0 * 2 + 1 }
-    var body: some View {
+
+	var body: some View {
 		List() {
-			ForEach(items, id: \.self) { item in
-				Text("Item \(item)")
-					.padding()
+			ForEach(0..<items.count) { i in
+				VStack {
+					Text("\(i == selected ? "> " : "")Item \(items[i])")
+						.frame(maxWidth: .infinity, alignment: .leading)
+					if (i == selected) {
+						Text("Detail 1\nDetail 2\nDetail 3")
+							.multilineTextAlignment(.trailing)
+							.frame(maxWidth: .infinity, alignment: .trailing)
+					}
+				}
+				.contentShape(Rectangle())
+				.onTapGesture {
+					print("Tap: \(selected) => \(i)")
+					selected = i
+				}
 			}
 		}
     }
