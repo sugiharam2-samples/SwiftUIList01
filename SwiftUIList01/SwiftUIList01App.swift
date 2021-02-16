689e19345a722a3fd22687c9ddb3e148999eb2d7
Mamoru Sugihara <sugiharam2@gmail.com>
2021-02-16 18:22:30 +0900

    Initial Commit

---
+++ SwiftUIList01/SwiftUIList01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIList01App.swift
+//  SwiftUIList01
+//
+//  Created by Mamoru Sugihara on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIList01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
